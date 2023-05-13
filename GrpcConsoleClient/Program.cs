52478b7fd82b86bf0f86377fc8b1244a6b3f1d45
Raixther <g.a.z@bk.ru>
2023-05-13 18:18:26 +0300

    Добавьте файлы проекта.

---
+++ GrpcConsoleClient/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcLearning;
+
+
+var channel = GrpcChannel.ForAddress("https://localhost:7214");
+var client = new Greeter.GreeterClient(channel);
+var result = await client.SayHelloAsync(new HelloRequest(){ Name= "Q"});
+
+Console.WriteLine(result.Message);      
