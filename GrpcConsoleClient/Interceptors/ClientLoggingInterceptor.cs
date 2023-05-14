431158096847bdcbf67b9fe4046b3d9676475c80
Raixther <g.a.z@bk.ru>
2023-05-14 08:03:07 +0300

    _

--- GrpcConsoleClient/Interceptors/ClientLoggingInterceptor.cs
+++ GrpcConsoleClient/Interceptors/ClientLoggingInterceptor.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Grpc.Core.Interceptors;
+﻿using Grpc.Core.Interceptors;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
-using static Grpc.Core.Interceptors.Interceptor;
 
 namespace GrpcConsoleClient.Interceptors
 {
--- GrpcConsoleClient/Program.cs
+++ GrpcConsoleClient/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core.Interceptors;
 using Grpc.Net.Client;
+using GrpcConsoleClient.Interceptors;
 using GrpcLearning;
-
+using Microsoft.Extensions.Logging;
 
 var channel = GrpcChannel.ForAddress("https://localhost:7214");
-var client = new Greeter.GreeterClient(channel);
-var result = await client.SayHelloAsync(new HelloRequest(){ Name= "Q"});
+var invoker = channel.Intercept(new ClientLoggingInterceptor(new LoggerFactory()));  
+var client = new Greeter.GreeterClient(invoker);
 
+
+var result = await client.SayHelloAsync(new HelloRequest(){ Name= "Q"});
 Console.WriteLine(result.Message);      
