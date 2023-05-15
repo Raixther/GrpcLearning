3a5d403de987d84630b5fd4977319969076c8aa3
Raixther <g.a.z@bk.ru>
2023-05-15 07:17:45 +0300

    health checks

--- GrpcLearning/Program.cs
+++ GrpcLearning/Program.cs
@@ -1,6 +1,8 @@
 using GrpcLearning.Interceptors;
 using GrpcLearning.Services;
 
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
@@ -10,11 +12,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc().AddServiceOptions<GreeterService>(cfg=>{
 cfg.Interceptors.Add<ServerLoggerInterceptor>();
 });
-
+builder.Services.AddGrpcHealthChecks().AddCheck("Sample", ()=>HealthCheckResult.Healthy());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 app.MapGrpcService<GreeterService>();
+app.MapGrpcHealthChecksService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
