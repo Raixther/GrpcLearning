c477b50cb9f9c87c84b638e8f61ab9b135adcfb3
Raixther <g.a.z@bk.ru>
2023-05-18 21:22:50 +0300

    update server healthcheck

--- GrpcLearning/Program.cs
+++ GrpcLearning/Program.cs
@@ -2,6 +2,7 @@ using GrpcLearning;
 using GrpcLearning.Interceptors;
 using GrpcLearning.Services;
 
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
@@ -16,16 +17,20 @@ cfg.Interceptors.Add<ServerLoggerInterceptor>();
 });
 builder.Services.AddScoped<GreeterService>();
 
-builder.Services.AddGrpcHealthChecks(cfg =>
-{
-	cfg.Services.MapService("greet.Greeter", s => s.Tags.Contains("greeter"));
-}).AddCheck<GreeterHealthCheck>("greeterCheck");
+//builder.Services.AddGrpcHealthChecks(cfg =>
+//{
+//	cfg.Services.MapService("greet.Greeter", s => s.Tags.Contains("greeter"));
+//}).AddCheck<GreeterHealthCheck>("greeterCheck");
+
+builder.Services.AddHealthChecks().AddCheck<GreeterHealthCheck>("greeterCheck") ;
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapGrpcService<GreeterService>();
-app.MapGrpcHealthChecksService().WithTags("greeter");
+//app.MapGrpcHealthChecksService().WithTags("greeter");
+app.MapHealthChecks("/health") ;
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
