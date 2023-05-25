8cdb36762a4d5adac34f9259deed81a7870f57fd
Raixther <g.a.z@bk.ru>
2023-05-25 19:09:35 +0300

    добавлен reader service(читает текст из файла)

---
+++ GrpcLearning/Services/Class.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess;
+
+using Grpc.Core;
+
+namespace GrpcLearning.Services
+{
+	public class ReaderService : Reader.ReaderBase
+	{
+		//протестировать
+		public override Task<GetDataFromTextResponse> GetDataFromText(GetDataFromTextRequest request, ServerCallContext context)
+		{
+			var resultRext = File.ReadAllText(request.Path);
+			var response = new GetDataFromTextResponse(){ Content = resultRext };
+			return Task.FromResult(response);
+		}
+	}
+}
